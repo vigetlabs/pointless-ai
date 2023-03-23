fbb6105fd62c459caf38ee47515c465fa9599fb2
Eric <ekfuhrmann@gmail.com>
2023-03-23 11:53:32 -0600

    Init config setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { remPair, rem } = require('@viget/tailwindcss-plugins/utilities/fns')
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
@@ -5,4 +7,42 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
   ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      primary: {
+        DEFAULT: '#994930',
+        hover: '#7A3A2B',
+        active: '#5B2B26',
+      },
+      secondary: {
+        DEFAULT: '#E2DAC6',
+      },
+    },
+    spacing: {
+      0: '0',
+      ...remPair(1),
+      ...remPair(2),
+      ...remPair(3),
+      ...remPair(4),
+      ...remPair(5),
+      ...remPair(6),
+      ...remPair(8),
+      ...remPair(10),
+      ...remPair(12),
+      ...remPair(14),
+      ...remPair(16),
+      ...remPair(18),
+      ...remPair(24),
+      ...remPair(28),
+      ...remPair(32),
+      ...remPair(36),
+      ...remPair(40),
+      ...remPair(44),
+      ...remPair(48),
+      ...remPair(56),
+    },
+  },
 }
