9cd81a066e7f81eb4a0743ba40b4bbb28d41cae1
Jacob Lowen <jacob.lowen@viget.com>
2023-03-23 14:29:39 -0400

    First pass at submit form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  # prompt_path POST /welcome/prompt  
+  def prompt_path
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  put "/home/prompt", to: "home#prompt", as: 'prompt'  
+  get "/home", to: "home#index"  
+
 
   # Defines the root path route ("/")
   root "home#index"
