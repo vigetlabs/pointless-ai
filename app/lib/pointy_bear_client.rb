15e98b61ffd6ef7b4f770556d00dacb00434ad25
mfigueredo <miguel.figueredo@viget.com>
2023-03-23 18:23:30 -0400

    Add ai_characters table
    - Flesh out openai pointy wrapper

--- app/lib/pointy_bear_client.rb
+++ app/lib/pointy_bear_client.rb
@@ -5,21 +5,30 @@ class PointyBearClient
   MODEL = "gpt-3.5-turbo".freeze
 
   class << self
-    def submit_prompt(prompt)
-      response = openai_client.chat(
+    def submit_prompt(user_submission)
+      # TODO error handling
+      # so far it timeouts, capped at 10s
+      openai_client.chat(
         parameters: {
           model: MODEL,
-          messages: [{role: "user", content: prompt}], # Required.
+          messages: [build_system_prompt_structure, {role: "user", content: user_submission}],
           temperature: 0.7
         }
       )
-      puts response
     end
 
     private
 
     def openai_client
       @client ||= OpenAI::Client.new
+    end
+
+    def character
+      @ai_character ||= AiCharacter.where(name: "Pointy Bear").first
+    end
+
+    def build_system_prompt_structure
+      {"role" => "system", "content" => character.prompt}
     end
   end
 end
--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -3,4 +3,5 @@ require "openai"
 OpenAI.configure do |config|
   # Follow the README under "Adding credentials" to add your own API key here
   config.access_token = Rails.application.credentials.openai.access_token
+  config.request_timeout = 10 # Optional
 end
