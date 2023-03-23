5302d37757ea083aed94ed1747ee499d1d4c3cf1
Joshua Pease <joshua.pease@viget.com>
2023-03-23 15:48:17 -0700

    Rough controller submissions and message typing animation.

---
+++ app/javascript/controllers/message_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from '@hotwired/stimulus'
+import Typed from 'typed.js'
+
+/**
+ * @description Controls the "typing effect" of new messages coming in
+ */
+export default class extends Controller {
+  static values = {
+    animate: {
+      type: Boolean,
+      default: false,
+    },
+  }
+
+  static targets = ['text']
+
+  initialize() {
+    console.log('message initialized')
+  }
+
+  connect() {
+
+    if (!this.animateValue) {
+      return
+    }
+    // Prevents a flash of the text before JS connects
+    this.element.hidden = false
+
+    const html = this.textTarget.innerHTML
+
+    this.textTarget.innerHTML = ''
+
+    new Typed(this.textTarget, {
+      strings: [html],
+      typeSpeed: 50,
+      showCursor: false,
+    })
+  }
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "typed.js", to: "https://ga.jspm.io/npm:typed.js@2.0.1-3.1/dist/typed.module.js"
