16611f1145b3d0c39272eb2c679719dee4689dbd
mfigueredo <miguel.figueredo@viget.com>
2023-03-23 13:41:29 -0400

    Update readme

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,5 +1,6 @@
 require "openai"
 
 OpenAI.configure do |config|
+  # Follow the README under "Adding credentials" to add your own API key here
   config.access_token = Rails.application.credentials.openai.access_token
 end
