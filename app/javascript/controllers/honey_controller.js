4e0a54b80a3388261119b4706622fadbd5830581
Eric <ekfuhrmann@gmail.com>
2023-03-23 22:01:31 -0600

    Added honey jar with logic bound to submit

---
+++ app/javascript/controllers/honey_controller.js
@@ -0,0 +1,66 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  static values = {
+    supply: {
+      type: Number,
+      default: 4,
+    },
+  }
+
+  static targets = ['jar']
+
+  connect() {
+    this.docStyle = document.documentElement.style
+  }
+
+  resupply() {
+    this.honeyBounce()
+
+    if (this.supplyValue == 4) return
+
+    this.supplyValue = 4
+    this.docStyle.setProperty('--honey-supply', '0')
+  }
+
+  submit(e) {
+    if (this.supplyValue == 0) {
+      e.preventDefault()
+    }
+
+    this.updateSupply()
+    this.honeyBounce()
+  }
+
+  honeyBounce() {
+    if (this.jarTarget.dataset.state === 'bounce') return
+
+    this.jarTarget.dataset.state = 'bounce'
+    setTimeout(() => {
+      delete this.jarTarget.dataset.state
+    }, 400)
+  }
+
+  updateSupply() {
+    switch (this.supplyValue) {
+      case 4:
+        this.docStyle.setProperty('--honey-supply', '35%')
+        --this.supplyValue
+        break
+      case 3:
+        this.docStyle.setProperty('--honey-supply', '50%')
+        --this.supplyValue
+        break
+      case 2:
+        this.docStyle.setProperty('--honey-supply', '75%')
+        --this.supplyValue
+        break
+      case 1:
+        this.docStyle.setProperty('--honey-supply', '100%')
+        --this.supplyValue
+        break
+      default:
+        break
+    }
+  }
+}
