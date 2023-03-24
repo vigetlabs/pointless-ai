9141103fff96c7116a666b9eaa6349e030f488e9
mfigueredo <miguel.figueredo@viget.com>
2023-03-24 01:39:23 -0400

    Fine tune controllers and add partials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,26 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_message_thread, only: [:index]
 
   def index
-    messages = session[:messages] || []
-    render "home/index", locals: {messages: messages}
-    # res = PointyBearClient.submit_prompt("Do you have any honey left?")
-    # render :json => {"data": res}
+  end
+
+  private
+
+  def message_thread
+    @message_thread ||= current_user.message_threads.first
+  end
+  helper_method :message_thread
+
+  def messages
+    @messages ||= message_thread.messages
+  end
+  helper_method :messages
+
+  def check_message_thread
+    # for mvp, create a message thread for when the user lands
+    if current_user.message_threads.empty?
+      current_user.message_threads.create
+    end
   end
 end
